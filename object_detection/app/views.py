f8ba948526aecc9a1f445b489cd5f5b08a3e9a3d
Rahul Murugesan <rahulcode97@gmail.com>
2025-02-25 17:27:19 +0530

    display uploaded image

--- object_detection/app/views.py
+++ object_detection/app/views.py
@@ -4,13 +4,14 @@ from app.forms import imageform
 # Create your views here.
 
 def home(request):
+    uploaded_image = None
     if(request.method == 'POST'):
         form = imageform(request.POST, request.FILES)
         if(form.is_valid()):
             filename = form.cleaned_data['image']
-            form.save()
+            uploaded_image = form.save()
         else:
             context = {'form': form}
             return render(request, "home.html", context)
-    context = {'form': imageform()}
+    context = {'form': imageform(), "uploaded_image": uploaded_image}
     return render(request, "home.html", context)
